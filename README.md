1bb218787d1782ddab1f73e67dc7decd3b5db2a3
LiaoshuaiMichael <1521652775@qq.com>
2020-03-31 12:37:50 +0800

    初始化后端项目

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# ClassManagementSystem
-oumi
+﻿班级管理系统后台项目
+
+项目统一采用IDAE
---
+++ src/test/java/com/tpg/cms/CmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpg.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
