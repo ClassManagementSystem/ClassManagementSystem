37a75bad55515f2a72653715b4cfbdb6bb31804b
LiaoshuaiMichael <1521652775@qq.com>
2020-05-14 21:24:49 +0800

    提交前后端登录接口

---
+++ src/main/java/com/tpg/cms/controller/LoginController.java
@@ -0,0 +1,22 @@
+package com.tpg.cms.controller;
+
+import com.tpg.cms.utils.Result;
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.*;
+
+@Api(tags = "用户登录管理")
+@RestController
+@RequestMapping("/clms")
+@CrossOrigin  //解决跨域
+public class LoginController {
+    //login
+    @PostMapping("login")
+    public Result login() {
+        return Result.OK().data("token","admin");
+    }
+    //info
+    @GetMapping("info")
+    public Result info() {
+        return Result.OK().data("roles","[admin]").data("name","admin").data("avatar","https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+    }
+}
